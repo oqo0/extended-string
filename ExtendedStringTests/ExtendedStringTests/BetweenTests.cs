9a8ef392bedcaaeae60b5c57a72fc201cb313b1a
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-12-20 20:13:29 +0300

    Add tests

---
+++ ExtendedStringTests/ExtendedStringTests/BetweenTests.cs
@@ -0,0 +1,29 @@
+using System;
+using NUnit.Framework;
+using ExtendedString;
+
+namespace ExtendedStringTests;
+
+[TestFixture]
+public partial class ExtendedString
+{
+    [Test]
+    [TestCase("#1#", '#', '#', "1")]
+    [TestCase("", '3', '#', null)]
+    public void StringBetweenChars(string testString, char openChar, char closeChar, string expectedResult)
+    {
+        string actualResult = testString.Between(openChar, closeChar);
+        
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+    
+    [Test]
+    [TestCase("#1@", "#", "@", "1")]
+    [TestCase("", "", "", null)]
+    public void StringBetweenStrings(string testString, string openStr, string closeStr, string expectedResult)
+    {
+        string actualResult = testString.Between(openStr, closeStr, StringComparison.Ordinal);
+        
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
