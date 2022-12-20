fd39befb7c3f422bb5010254be4711202db3a041
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-12-20 23:13:10 +0300

    Add CountOccurrence tests

--- ExtendedStringTests/ExtendedStringTests/CountOccurrenceTests.cs
+++ ExtendedStringTests/ExtendedStringTests/CountOccurrenceTests.cs
@@ -1,6 +1,18 @@
+using NUnit.Framework;
+using ExtendedString;
+
 namespace ExtendedStringTests;
 
-public class CountOccurrenceTests
+[TestFixture]
+public partial class ExtendedString
 {
-    
+    [Test]
+    [TestCase("2", "2", 1)]
+    [TestCase("", "2", null)]
+    public void CountOccurrence(string testString, string countedString, int expectedResult)
+    {
+        int actualResult = testString.CountOccurrence(countedString);
+        
+        Assert.AreEqual(expectedResult, actualResult);
+    }
 }
