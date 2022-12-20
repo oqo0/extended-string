5443bdce446eb155f4ea14fec1b1ab3a583d10c5
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-12-20 22:58:40 +0300

    Feat: Add CountOccurrence method

---
+++ ExtendedString/scr/CountOccurrence.cs
@@ -0,0 +1,30 @@
+namespace ExtendedString;
+
+public static partial class ExtendedString
+{
+    public static int CountOccurrence(this string str, string line)
+    {
+        return CountOccurrence(str, line);
+    }
+    
+    public static int CountOccurrence(this string str, string line, StringComparison stringComparison)
+    {
+        if (String.IsNullOrEmpty(str))
+            return 0;
+        if (line == null)
+            throw new ArgumentNullException(nameof(line));
+
+        int stringIndex = str.IndexOf(line, stringComparison);
+        
+        int count = 0;
+
+        while (stringIndex != -1)
+        {
+            count++;
+            
+            stringIndex = str.IndexOf(line, stringIndex + line.Length ,stringComparison);
+        }
+
+        return count;
+    }
+}
