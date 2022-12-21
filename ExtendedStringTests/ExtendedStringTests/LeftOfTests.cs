bab6da6fffdaaaf7fa53fe9b8caa7cefa72692ba
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-12-21 17:20:58 +0300

    Add LeftOf method tests

--- ExtendedStringTests/ExtendedStringTests/LeftOfTests.cs
+++ ExtendedStringTests/ExtendedStringTests/LeftOfTests.cs
@@ -1,6 +1,33 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using ExtendedString;
+
 namespace ExtendedStringTests;
 
-public class LeftOfTests
+[TestFixture]
+public partial class ExtendedString
 {
+    [Test]
+    [TestCase("01", '1', "0")]
+    [TestCase("1", '1', null)]
+    [TestCase("", '1', null)]
+    public void LeftOfChar(string testString, char character, string expectedResult)
+    {
+        var actualResult = testString.LeftOf(character);
+        
+        Assert.AreEqual(expectedResult, actualResult);
+    }
     
+    [Test]
+    [TestCase("01", "1", "0")]
+    [TestCase("0012", "12", "00")]
+    [TestCase("1", "1", null)]
+    [TestCase("", "1", null)]
+    public void LeftOfString(string testString, string value, string expectedResult)
+    {
+        var actualResult = testString.LeftOf(value);
+        
+        Assert.AreEqual(expectedResult, actualResult);
+    }
 }
