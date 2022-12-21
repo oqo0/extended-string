bc856c11de8311a1418487a2fa6c92aaee4d29ca
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-12-21 18:17:47 +0300

    Add RightOf tests

--- ExtendedStringTests/ExtendedStringTests/RightOfTests.cs
+++ ExtendedStringTests/ExtendedStringTests/RightOfTests.cs
@@ -1,6 +1,31 @@
+using NUnit.Framework;
+using ExtendedString;
+
 namespace ExtendedStringTests;
 
-public class RightOfTests
+[TestFixture]
+public partial class ExtendedString
 {
+    [Test]
+    [TestCase("10", '1', "0")]
+    [TestCase("1", '1', null)]
+    [TestCase("", '1', null)]
+    public void RightOfChar(string testString, char character, string expectedResult)
+    {
+        var actualResult = testString.RightOf(character);
+        
+        Assert.AreEqual(expectedResult, actualResult);
+    }
     
+    [Test]
+    [TestCase("10", "1", "0")]
+    [TestCase("1200", "12", "00")]
+    [TestCase("1", "1", null)]
+    [TestCase("", "1", null)]
+    public void RightOfString(string testString, string value, string expectedResult)
+    {
+        var actualResult = testString.RightOf(value);
+        
+        Assert.AreEqual(expectedResult, actualResult);
+    }
 }
