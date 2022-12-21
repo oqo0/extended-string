c7232ca4510302c887414b67894e07605f1a9b51
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-12-21 17:13:35 +0300

    Add method overloads

--- ExtendedString/scr/RightOf.cs
+++ ExtendedString/scr/RightOf.cs
@@ -1,7 +1,14 @@
+using Microsoft.VisualBasic;
+
 namespace ExtendedString;
 
 public static partial class ExtendedString
 {
+    public static string RightOf(this string str, char character)
+    {
+        return RightOf(str, character, 0);
+    }
+    
     public static string RightOf(this string str, char character, int occurrenceIndex)
     {
         if (String.IsNullOrEmpty(str))
@@ -18,6 +25,38 @@ public static partial class ExtendedString
         while (occuranceCount < occurrenceIndex && characterIndex != -1)
         {
             characterIndex = str.IndexOf(character, characterIndex + 1);
+            occuranceCount++;
+        }
+        
+        return str.Substring(characterIndex + 1, str.Length - characterIndex - 1);
+    }
+
+    public static string RightOf(this string str, string value)
+    {
+        return RightOf(str, value, 0);
+    }
+    
+    public static string RightOf(this string str, string value, int occurrenceIndex)
+    {
+        return RightOf(str, value, occurrenceIndex, StringComparison.Ordinal);
+    }
+    
+    public static string RightOf(this string str, string value, int occurrenceIndex, StringComparison stringComparison)
+    {
+        if (String.IsNullOrEmpty(str))
+            return null;
+        
+        int characterIndex = str.IndexOf(value, stringComparison);
+        if (characterIndex == -1 || characterIndex == 0)
+        {
+            return null;
+        }
+
+        int occuranceCount = 0;
+        
+        while (occuranceCount < occurrenceIndex && characterIndex != -1)
+        {
+            characterIndex = str.IndexOf(value, characterIndex + 1, stringComparison);
             occuranceCount++;
         }
         
