588172c1096736708833de26de59b4652924ad6a
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-12-23 22:06:56 +0300

    Add XML documentation comments

--- ExtendedString/scr/Between.cs
+++ ExtendedString/scr/Between.cs
@@ -2,11 +2,30 @@ namespace ExtendedString;
 
 public static partial class ExtendedString
 {
+    /// <summary>
+    /// Extracts the part of the string that is between the first two occurrences of characters.
+    /// </summary>
+    /// <param name="str">Input string.</param>
+    /// <param name="character">Character to use for substring scope.</param>
+    /// <returns>
+    /// A substring starting from the first occurrence of the character to the second occurrence of the character.
+    /// Returns null if string is null or empty.
+    /// </returns>
     public static string Between(this string str, char character)
     {
         return Between(str, character, character);
     }
     
+    /// <summary>
+    /// Extracts the part of the string that is between of openCharacter and closeCharacter.
+    /// </summary>
+    /// <param name="str">Input string.</param>
+    /// <param name="openCharacter">Character to use to open substring scope.</param>
+    /// <param name="closeCharacter">Character to use to close substring scope.</param>
+    /// <returns>
+    /// A substring starting from the openCharacter to the closeCharacter.
+    /// Returns null if string is null or empty.
+    /// </returns>
     public static string Between(this string str, char openCharacter, char closeCharacter)
     {
         if (String.IsNullOrEmpty(str))
@@ -26,17 +45,47 @@ public static partial class ExtendedString
     
         return str.Substring(openCharacterEntryIndex + 1, closeCharacterEntryIndex - openCharacterEntryIndex - 1);
     }
-
+    
+    /// <summary>
+    /// Extracts the part of the string that is between the first two occurrences of openString.
+    /// </summary>
+    /// <param name="str">Input string.</param>
+    /// <param name="openString">String to use for substring scope.</param>
+    /// <returns>
+    /// A substring starting from the first occurrence of the openString to the second occurrence of the openString.
+    /// Returns null if string is null or empty.
+    /// </returns>
     public static string Between(this string str, string openString)
     {
         return Between(str, openString, openString, StringComparison.Ordinal);
     }   
     
+    /// <summary>
+    /// Extracts the part of the string that is between the first two occurrences of openString.
+    /// </summary>
+    /// <param name="str">Input string.</param>
+    /// <param name="openString">String to use for substring scope.</param>
+    /// <param name="stringComparison">StringComparison to use to search for open string occurrence.</param>
+    /// <returns>
+    /// A substring starting from the first occurrence of the openString to the second occurrence of the openString.
+    /// Returns null if string is null or empty.
+    /// </returns>
     public static string Between(this string str, string openString, StringComparison stringComparison)
     {
         return Between(str, openString, openString, stringComparison);
     }
     
+    /// <summary>
+    /// Extracts the part of the string that is between the first two occurrences of openString.
+    /// </summary>
+    /// <param name="str">Input string.</param>
+    /// <param name="openString">String to use to open substring scope.</param>
+    /// <param name="closeString">String to use to close substring scope.</param>
+    /// <param name="stringComparison">StringComparison to use to search for open string occurrence.</param>
+    /// <returns>
+    /// A substring starting from the first occurrence of the openString to the second occurrence of the openString.
+    /// Returns null if string is null or empty.
+    /// </returns>
     public static string Between(this string str, string openString, string closeString, StringComparison stringComparison)
     {
         if (String.IsNullOrEmpty(str))
