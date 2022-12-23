5f2115d3e9db4c57002fc31b03237ca4053dcfdd
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-12-23 18:35:49 +0300

    Add Reverse tests

--- ExtendedStringTests/ExtendedStringTests/ReverseTests.cs
+++ ExtendedStringTests/ExtendedStringTests/ReverseTests.cs
@@ -1,6 +1,20 @@
+using NUnit.Framework;
+using ExtendedString;
+
 namespace ExtendedStringTests;
 
-public class ReverseTests
+[TestFixture]
+public partial class ExtendedString
 {
-    
+    [Test]
+    [TestCase(null, null)]
+    [TestCase("", "")]
+    [TestCase("1", "1")]
+    [TestCase("123", "321")]
+    public void ReverseString(string testString, string expectedResult)
+    {
+        var actualResult = testString.Reverse();
+        
+        Assert.AreEqual(expectedResult, actualResult);
+    }
 }
