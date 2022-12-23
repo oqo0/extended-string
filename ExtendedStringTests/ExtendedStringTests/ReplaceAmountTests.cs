cf5d3c10c7f6ec6d1250f227c8c087db70a78ec0
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-12-23 18:09:42 +0300

    Add ReplaceAmount tests

--- ExtendedStringTests/ExtendedStringTests/ReplaceAmountTests.cs
+++ ExtendedStringTests/ExtendedStringTests/ReplaceAmountTests.cs
@@ -1,6 +1,20 @@
+using NUnit.Framework;
+using ExtendedString;
+
 namespace ExtendedStringTests;
 
-public class ReplaceAmountTests
+[TestFixture]
+public partial class ExtendedString
 {
-    
+    [Test]
+    [TestCase("_11", "1", 1, "_1")]
+    [TestCase("_11", "1", 2, "_")]
+    [TestCase("_11", "1", 3, "_")]
+    [TestCase("", "1", 3, "")]
+    public void ReplaceString(string testString, string value, int replaceAmount, string expectedResult)
+    {
+        var actualResult = testString.ReplaceAmount(value, replaceAmount);
+        
+        Assert.AreEqual(expectedResult, actualResult);
+    }
 }
