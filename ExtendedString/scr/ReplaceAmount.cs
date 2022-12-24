3f841cbcfb3f026356f495a34c41987182b66973
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-12-24 16:11:22 +0300

    Add XML documentation comments

--- ExtendedString/scr/ReplaceAmount.cs
+++ ExtendedString/scr/ReplaceAmount.cs
@@ -2,10 +2,30 @@ namespace ExtendedString;
 
 public static partial class ExtendedString
 {
+    /// <summary>
+    /// Replaces a specified amount of string occurences in string.
+    /// </summary>
+    /// <param name="str">Input string.</param>
+    /// <param name="value">Substring to replace.</param>
+    /// <param name="amount">Amount of substrings to replace.</param>
+    /// <returns>
+    /// Returns string with specified amount of substrings replaces.
+    /// </returns>
     public static string ReplaceAmount(this string str, string value, int amount)
     {
         return ReplaceAmount(str, value, amount, StringComparison.Ordinal);
     }
+    
+    /// <summary>
+    /// Replaces a specified amount of string occurences in string.
+    /// </summary>
+    /// <param name="str">Input string.</param>
+    /// <param name="value">Substring to replace.</param>
+    /// <param name="amount">Amount of substrings to replace.</param>
+    /// <param name="stringComparison">StringComparison to use to search for substring.</param>
+    /// <returns>
+    /// Returns string with specified amount of substrings replaces.
+    /// </returns>
     public static string ReplaceAmount(this string str, string value, int amount, StringComparison stringComparison)
     {
         if (String.IsNullOrEmpty(str))
