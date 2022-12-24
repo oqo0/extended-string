b1d7cb680ea8bcd6c5a5ccb7137805d5e64f952a
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-12-24 16:12:09 +0300

    Add XML documentation comments

--- ExtendedString/scr/Reverse.cs
+++ ExtendedString/scr/Reverse.cs
@@ -2,6 +2,13 @@ namespace ExtendedString;
 
 public static partial class ExtendedString
 {
+    /// <summary>
+    /// Reverses a string.
+    /// </summary>
+    /// <param name="str">Input string.</param>
+    /// <returns>
+    /// Returns a reversed string.
+    /// </returns>
     public static string Reverse(this string str)
     {
         if (str == null)
