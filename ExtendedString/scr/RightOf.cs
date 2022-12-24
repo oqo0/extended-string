c946e801b55679dbb4c2a94774dc2e04d3cf4de3
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-12-24 16:09:10 +0300

    Add XML documentation comments

--- ExtendedString/scr/RightOf.cs
+++ ExtendedString/scr/RightOf.cs
@@ -1,14 +1,29 @@
-using Microsoft.VisualBasic;
-
 namespace ExtendedString;
 
 public static partial class ExtendedString
 {
+    /// <summary>
+    /// Exctacts the part of string at right from a character.
+    /// </summary>
+    /// <param name="str">Input string.</param>
+    /// <param name="character">Char to use to start a substring.</param>
+    /// <returns>
+    /// Returns a string to the right of a specified character.
+    /// </returns>
     public static string RightOf(this string str, char character)
     {
         return RightOf(str, character, 0);
     }
     
+    /// <summary>
+    /// Exctacts the part of string at right from a character.
+    /// </summary>
+    /// <param name="str">Input string.</param>
+    /// <param name="character">Char to use to start a substring.</param>
+    /// <param name="occurrenceIndex">Index of character occurrence which will be used as the start of the substring.</param>
+    /// <returns>
+    /// Returns a string to the right of a specified character.
+    /// </returns>
     public static string RightOf(this string str, char character, int occurrenceIndex)
     {
         if (String.IsNullOrEmpty(str))
@@ -31,16 +46,43 @@ public static partial class ExtendedString
         return str.Substring(characterIndex + 1, str.Length - characterIndex - 1);
     }
 
+    /// <summary>
+    /// Exctacts the part of string at right from a string.
+    /// </summary>
+    /// <param name="str">Input string.</param>
+    /// <param name="value">String to use to start a substring.</param>
+    /// <returns>
+    /// Returns a string to the right of a specified string.
+    /// </returns>
     public static string RightOf(this string str, string value)
     {
         return RightOf(str, value, 0);
     }
     
+    /// <summary>
+    /// Exctacts the part of string at right from a string.
+    /// </summary>
+    /// <param name="str">Input string.</param>
+    /// <param name="value">String to use to start a substring.</param>
+    /// <param name="occurrenceIndex">Index of string occurrence end of which will be used as the start of the substring.</param>
+    /// <returns>
+    /// Returns a string to the right of a specified string.
+    /// </returns>
     public static string RightOf(this string str, string value, int occurrenceIndex)
     {
         return RightOf(str, value, occurrenceIndex, StringComparison.Ordinal);
     }
     
+    /// <summary>
+    /// Exctacts the part of string at right from a string.
+    /// </summary>
+    /// <param name="str">Input string.</param>
+    /// <param name="value">String to use to start a substring.</param>
+    /// <param name="occurrenceIndex">Index of string occurrence end of which will be used as the start of the substring.</param>
+    /// <param name="stringComparison">StringComparison to use to search for value occurrence.</param>
+    /// <returns>
+    /// Returns a string to the right of a specified string.
+    /// </returns>
     public static string RightOf(this string str, string value, int occurrenceIndex, StringComparison stringComparison)
     {
         if (String.IsNullOrEmpty(str))
