f854c95168ea6a27b604c438a2068dd10a110958
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-12-24 16:02:17 +0300

    Add XML documentation comments

--- ExtendedString/scr/LeftOf.cs
+++ ExtendedString/scr/LeftOf.cs
@@ -2,11 +2,28 @@ namespace ExtendedString;
 
 public static partial class ExtendedString
 {
+    /// <summary>
+    /// Exctacts the part of string left from a character.
+    /// </summary>
+    /// <param name="str">Input string.</param>
+    /// <param name="character">Char to use to end substring.</param>
+    /// <returns>
+    /// Returns a string to the left of a specified character.
+    /// </returns>
     public static string LeftOf(this string str, char character)
     {
         return LeftOf(str, character, 0);
     }
     
+    /// <summary>
+    /// Exctacts the part of string left from a character.
+    /// </summary>
+    /// <param name="str">Input string.</param>
+    /// <param name="character">Char to use to end substring.</param>
+    /// <param name="occurrenceIndex">Index of character occurrence which well be used as the end of the substring.</param>
+    /// <returns>
+    /// Returns a string to the left of a specified character.
+    /// </returns>
     public static string LeftOf(this string str, char character, int occurrenceIndex)
     {
         if (String.IsNullOrEmpty(str))
@@ -29,16 +46,43 @@ public static partial class ExtendedString
         return str.Substring(0, characterIndex);
     }
 
+    /// <summary>
+    /// Exctacts the part of string left from a string.
+    /// </summary>
+    /// <param name="str">Input string.</param>
+    /// <param name="value">String to use to start substring.</param>
+    /// <returns>
+    /// Returns a string to the left of a specified string.
+    /// </returns>
     public static string LeftOf(this string str, string value)
     {
         return LeftOf(str, value, 0);
     }
     
+    /// <summary>
+    /// Exctacts the part of string left from a string.
+    /// </summary>
+    /// <param name="str">Input string.</param>
+    /// <param name="value">String to use to start substring.</param>
+    /// <param name="occurrenceIndex">Index of character occurrence which well be used as the end of the substring.</param>
+    /// <returns>
+    /// Returns a string to the left of a specified string.
+    /// </returns>
     public static string LeftOf(this string str, string value, int occurrenceIndex)
     {
         return LeftOf(str, value, occurrenceIndex, StringComparison.Ordinal);
     }
-    
+
+    /// <summary>
+    /// Exctacts the part of string left from a string.
+    /// </summary>
+    /// <param name="str">Input string.</param>
+    /// <param name="value">String to use to start substring.</param>
+    /// <param name="occurrenceIndex">Index of character occurrence which well be used as the end of the substring.</param>
+    /// <param name="stringComparison">StringComparison to use to search for value occurrence.</param>
+    /// <returns>
+    /// Returns a string to the left of a specified string.
+    /// </returns>
     public static string LeftOf(this string str, string value, int occurrenceIndex, StringComparison stringComparison)
     {
         if (String.IsNullOrEmpty(str))
