2c8e05bd0b66df994aa53abc8ecdfae5e54e65cf
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-12-24 15:03:14 +0300

    Add XML documentation comments

--- ExtendedString/scr/CountOccurrence.cs
+++ ExtendedString/scr/CountOccurrence.cs
@@ -2,11 +2,28 @@ namespace ExtendedString;
 
 public static partial class ExtendedString
 {
+    /// <summary>
+    /// Counts line occurrences in string
+    /// </summary>
+    /// <param name="str">Input string.</param>
+    /// <param name="line">Line to count occurences of.</param>
+    /// <returns>
+    /// Returns an amount of line occurrences in a string.
+    /// </returns>
     public static int CountOccurrence(this string str, string line)
     {
         return CountOccurrence(str, line, StringComparison.Ordinal);
     }
     
+    /// <summary>
+    /// Counts line occurrences in string
+    /// </summary>
+    /// <param name="str">Input string.</param>
+    /// <param name="line">Line to count occurences of.</param>
+    /// <param name="stringComparison">StringComparison to use to search for line.</param>
+    /// <returns>
+    /// Returns an amount of line occurrences in a string.
+    /// </returns>
     public static int CountOccurrence(this string str, string line, StringComparison stringComparison)
     {
         if (String.IsNullOrEmpty(str))
