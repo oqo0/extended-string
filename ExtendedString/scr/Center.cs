24499e57e5c5290c252ea21f8620aefb4497e8b6
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-12-24 14:57:39 +0300

    Add XML documentation comments

--- ExtendedString/scr/Center.cs
+++ ExtendedString/scr/Center.cs
@@ -4,6 +4,14 @@ namespace ExtendedString;
 
 public static partial class ExtendedString
 {
+    /// <summary>
+    /// Centers a string.
+    /// </summary>
+    /// <param name="str">Input string.</param>
+    /// <param name="width">Maximal width of a field</param>
+    /// <returns>
+    /// Returns the string centered in a string of specified width.
+    /// </returns>
     public static string Center(this string str, int width)
     {
         if (str.Length >= width)
